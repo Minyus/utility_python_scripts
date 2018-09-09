20b66f2d1897b01aa0c46e2c7fe3f4660ee95bc1
Yusuke Minami <33908456+Minyus@users.noreply.github.com>
2018-09-09 19:50:32 +0800

    Changed the numbers of digits for general use

--- print_progress.py
+++ print_progress.py
@@ -15,14 +15,21 @@ def print_progress(iteration, total_iterations, update_interval_sec = 1.0):
         t_elapsed = t - _time_started    
         i_ = i + 1
         progress = i_ / total_iterations
+        progress_pct = 100 * progress
         t_est_total = t_elapsed / progress
         t_est_remained = t_est_total - t_elapsed
-        sys.stdout.write('\r Progress:{0: 5.2f} % '.format(100 * i_ / total_iterations) \
-                         + ' | Processed:{0: 7d}/{1: 6d}'.format(i_, (total_iterations))\
-                         + ' | Elapsed: {0: 6.0f} sec'.format(t_elapsed)\
-                         + ' | Est total: {0: 8.0f} sec'.format(t_est_total)\
-                         + ' | Est remained: {0: 8.0f} sec'.format(t_est_remained)\
+        sys.stdout.write(f'\rProgress:{progress_pct: 5.2f} % ' \
+                         + f' | Processed:{i_: d}/{total_iterations: d} ' \
+                         + f' | Elapsed: {t_elapsed: 8.0f} sec' \
+                         + f' | Est total: {t_est_total: 8.0f} sec' \
+                         + f' | Est remained: {t_est_remained: 8.0f} sec' \
                         )
         sys.stdout.flush()
     if i == (total_iterations - 1):
         print()
+        
+### Example ###
+if __name__ == "__main__":
+    total_iterations = 100
+    for i in range(total_iterations):
+        print_progress(i, total_iterations)
